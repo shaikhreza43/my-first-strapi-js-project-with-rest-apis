775295c788a009808edca59c03a9e6b9f4069fc8
shaikhreza43 <reza.rk786@gmail.com>
2020-09-21 01:57:57 +0530

    Connected to Mongo DB ATLAS

--- config/database.js
+++ config/database.js
@@ -2,13 +2,13 @@ module.exports = ({ env }) => ({
   defaultConnection: 'default',
   connections: {
     default: {
-      connector: 'bookshelf',
+      connector: 'mongoose',
       settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        "uri":"mongodb://ahmed:ahmed1234@resolute-ai-cms-cluster-shard-00-00.hmevd.mongodb.net:27017,resolute-ai-cms-cluster-shard-00-01.hmevd.mongodb.net:27017,resolute-ai-cms-cluster-shard-00-02.hmevd.mongodb.net:27017/test-cms?ssl=true&replicaSet=atlas-crldd6-shard-0&authSource=admin&retryWrites=true&w=majority"
       },
       options: {
         useNullAsDefault: true,
+        "ssl":true
       },
     },
   },
